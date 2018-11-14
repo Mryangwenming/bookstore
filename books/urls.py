0d98ed0743b8408bf772cc214cc6e99c9f6c172f
Mryangwenming <1593342097@qq.com>
2018-11-14 16:37:00 +0800

    books list

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from .views import book_list,book_detail
 
 urlpatterns = [
-    url(r'book_list/$',book_list,name='book_list'),
+    url(r'book_list/(?P<type_id>\d+)/(?P<page>\d+)/$', book_list, name='book_list'),
     url(r'book_detail/(\d+)/$',book_detail,name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,13 +1,47 @@
 from django.shortcuts import render,redirect,reverse
 from .models import Books
 from .enums import *
+from django.core.paginator import Paginator
 
 # Create your views here.
 
-def book_list(request):
-        
-    return render(request,'list.html')
+def book_list(request,type_id,page):
+    sort = request.GET.get('sort','default')
+    if int(type_id) not in BOOKS_TYPE.keys():
+        return redirect(reverse('index'))
 
+    books_li = Books.objects.get_books_by_type(type_id=type_id,sort=sort)
+
+    paginator = Paginator(books_li,1)
+    num_pages = paginator.num_pages
+    if page == '' or int(page)>num_pages:
+        page = 1
+    else:
+        page = int(page)
+    books_li = paginator.page(page)
+    
+    if num_pages < 5:
+        pages = range(1,num_pages+1)
+    elif page <=3:
+        pages = range(1,6)
+    elif num_pages - page <=2:
+        pages = range(num_pages-4,num_pages+1)
+    else:
+        pages = range(page-2,page+3)
+
+    books_new = Books.objects.get_books_by_type(type_id=type_id,limit=2,sort='new')
+    type_title = BOOKS_TYPE[int(type_id)]
+    context = {
+        'books_li':books_li,
+        'books_new':books_new,
+        'type_id':type_id,
+        'sort':sort,
+        'type_title':type_title,
+        'pages':pages
+    } 
+    
+    return render(request,'list.html',context)
+    
 
 def book_detail(request,book_id):
     books = Books.objects.get_books_by_id(books_id=book_id)
