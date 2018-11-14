640ce836f9dd95199fc41cd3aa0873fc1d297a6b
Mryangwenming <1593342097@qq.com>
2018-11-14 09:10:32 +0800

    detail

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import book_list
+from .views import book_list,book_detail
 
 urlpatterns = [
-    url(r'book_list/$',book_list,name='book_list')
+    url(r'book_list/$',book_list,name='book_list'),
+    url(r'book_detail/(\d+)/$',book_detail,name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from .models import Books
+from .enums import *
 
 # Create your views here.
 
 def book_list(request):
         
     return render(request,'list.html')
+
+
+def book_detail(request,book_id):
+    books = Books.objects.get_books_by_id(books_id=book_id)
+    if books is None:
+        return redirect(reverse('index'))
+    books_li = Books.objects.get_books_by_type(type_id=books.type_id,limit=2,sort='new')
+    type_title = BOOKS_TYPE[books.type_id]
+
+    context = {'books':books,'books_li':books_li,'type_title':type_title}
+    
+    return render(request,'detail.html',context)
+    
